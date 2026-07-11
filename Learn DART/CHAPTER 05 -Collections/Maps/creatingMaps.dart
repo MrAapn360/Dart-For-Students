282b0c3f093a455437d89f18d181d33373375cc6
MrAapn360 <mraapn360@gmail.com>
2026-07-11 23:41:22 +0500

    creatingmaps File Added

---
+++ Learn DART/CHAPTER 05 -Collections/Maps/creatingMaps.dart
@@ -0,0 +1,11 @@
+void main() {
+  // Method 1
+  Map<String, int> marks = {"Ali": 80, "Ahmed": 90};
+  Map<String, String> student = {"name": "Sami", "city": "Abbottabad"};
+
+  // Method 2
+  var data = {"a": 1, "b": 2};
+
+  // Method 3
+  Map<String, String> emptyMap = {};
+}
